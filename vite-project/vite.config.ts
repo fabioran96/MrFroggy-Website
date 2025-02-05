851ee9fdc0e5a1b7d9af48c0d97b33236c200eda
Fabio Ranocchiari <fabio.ranocchiari@gmail.com>
2025-02-05 18:43:55 +0100

    Update vite.config.ts

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   base: "./", // Imposta la base relativa per il deploy su Vercel
   build: {
     outDir: "dist", // Assicura che la build venga salvata nella cartella giusta
     rollupOptions: {
-      external: ["bootstrap/dist/css/bootstrap.min.css"], // Evita di cercare il CSS durante il bundle
+      external: mode === "production" ? ["bootstrap/dist/css/bootstrap.min.css"] : [], // Evita di cercare il CSS durante il bundle
     },
   },
   server: {
@@ -20,4 +20,4 @@ export default defineConfig({
       "@": "/src", // Shortcut per importazioni dalla cartella src
     },
   },
-})
+}))
