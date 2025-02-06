0aa242cca82f1cdff111946b7f7f489ea6c96cf9
Fabio Ranocchiari <fabio.ranocchiari@gmail.com>
2025-02-06 13:55:47 +0100

    Update vite.config.ts

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "./", // Imposta la base relativa per il deploy su Vercel
   build: {
     outDir: "dist", // Assicura che la build venga salvata nella cartella giusta
     rollupOptions: {
